b1c90581470115bd02a02cbf90ee16c795485249
Wataru Yamashita <yamashita@nippontect.co.jp>
2021-06-06 19:59:12 +0900

    Threadの開始

--- C-practice/Program.cs
+++ C-practice/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+//using System.Diagnostics;
 
 namespace C_practice
 {
@@ -10,7 +12,27 @@ namespace C_practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            var th = new Thread(ExecuteInForeground);　//Threadクラスの生成
+            th.Start(); //Threadの開始
+            Thread.Sleep(1000); //1秒待機
+            Console.WriteLine("Main thread({0}) exiting...",
+                                Thread.CurrentThread.ManagedThreadId);
+        }
+        private static void ExecuteInForeground()
+        {
+            var sw = System.Diagnostics.Stopwatch.StartNew();
+            Console.WriteLine("Thread{0}: {1}, Priority{2}",
+                               Thread.CurrentThread.ManagedThreadId,
+                               Thread.CurrentThread.ThreadState,
+                               Thread.CurrentThread.Priority);
+            do
+            {
+                Console.WriteLine("Thread {0}: Elapsed {1:N2} seconds",
+                                  Thread.CurrentThread.ManagedThreadId,
+                                  sw.ElapsedMilliseconds / 1000.0);
+                Thread.Sleep(500);
+            } while (sw.ElapsedMilliseconds <= 5000);
+            sw.Stop();
         }
     }
 }
