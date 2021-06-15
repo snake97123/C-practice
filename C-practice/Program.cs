779548cd0d6d9d50b7f074c21999a59e3763dacf
snake97123 <snake12379@yahoo.co.jp>
2021-06-15 17:45:15 +0900

    匿名クラスの理解

--- C-practice/Program.cs
+++ C-practice/Program.cs
@@ -2,7 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace MyConsoleApp
+namespace Linqpractice
 {
-   
+    class Mainclass
+    {
+        public static void Main(string[] args)
+        {
+            var HumanList = new[]
+            {
+                new { Name = "山下", age = 44, weight = 50},
+                new { Name = "山田", age = 58, weight = 90},
+                new { Name = "吉田", age = 90, weight = 38},
+                new { Name = "花子", age = 28, weight = 35},
+            };
+
+            foreach (var human in HumanList)
+            {
+                Console.WriteLine(human);
+            }
+            Console.ReadLine();
+        }
+    }
 }
+
+    
